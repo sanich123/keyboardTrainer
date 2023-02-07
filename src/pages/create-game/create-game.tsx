62724993be35393aace8a8e6dab01081bcae44cd
Maxim Predybailo <predmaxim@ya.ru>
2023-02-07 21:32:44 +0500

    refactor: TrafficLight function on create-game

--- src/pages/create-game/create-game.tsx
+++ src/pages/create-game/create-game.tsx
@@ -1,36 +1,46 @@
 import Settings, { TrafficLight } from '../../components';
+import { langsData } from '../../components/Settings';
 import { TrafficLightProps } from '../../components/TrafficLight';
+import { LANG_VALUES } from '../../utils/const';
+import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 import styles from './create-game.module.scss';
 
 export default function CreateGamePage() {
-  const redProps: TrafficLightProps = {
+  const { isRu } = useThemeLang();
+  const lang = isRu ? LANG_VALUES.ru : LANG_VALUES.en;
+
+  const elapsedTime: TrafficLightProps = {
+    id: 1,
     color: 'red',
     textInfo: '01:22',
-    textDesc: 'Elapsed time',
-    unit: 'min',
+    textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).elapsedTime}`,
+    unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).elapsedTimeUnit}`,
   };
 
-  const greenProps: TrafficLightProps = {
+  const speed: TrafficLightProps = {
+    id: 2,
+    color: 'yellow',
+    textInfo: '100',
+    textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).speed}`,
+    unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).speedUnit}`,
+  };
+
+  const accuracy: TrafficLightProps = {
+    id: 3,
     color: 'green',
     textInfo: '120',
-    textDesc: 'Speed',
-    unit: 'chars/min',
+    textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).accuracy}`,
+    unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).accuracyUnit}`,
   };
 
-  const yellowProps: TrafficLightProps = {
-    color: 'yellow',
-    textInfo: '100%',
-    textDesc: 'Accuracy',
-    unit: '%',
-  };
+  const trafficLightProps: TrafficLightProps[] = [elapsedTime, speed, accuracy];
+
 
   return (
     <div id="CreateGamePage">
       <Settings />
       <div className={`trafficLight ${styles.trafficLight}`}>
-        <TrafficLight {...redProps} />
-        <TrafficLight {...greenProps} />
-        <TrafficLight {...yellowProps} />
+        {trafficLightProps.map((prop: TrafficLightProps) => <TrafficLight key={`${prop.id}`} {...prop} />)}
       </div>
     </div>
   );
