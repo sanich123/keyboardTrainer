85726741141ef1751164b94942017ec2dc84c437
Maxim Predybailo <predmaxim@ya.ru>
2023-02-07 12:24:16 +0500

    refactor: clean code for LangSwitch component

--- src/components/Settings/LangSwitch/LangSwitch.tsx
+++ src/components/Settings/LangSwitch/LangSwitch.tsx
@@ -3,17 +3,13 @@ import { langsData } from './langsData';
 import { RiTranslate2 } from 'react-icons/ri';
 import style from './LangSwitch.module.scss';
 import { SettingsProps } from '../../../utils/types/types';
+import { LANG_VALUES } from '../../../utils/const';
 
-export default function LangSwitch({ dispatch, theme, lang }: SettingsProps) {
-  // const { dispatch, isRu, isLight }: { dispatch: Dispatch<AnyAction>; isRu: boolean; isLight: boolean; } = useThemeLang();
-  // const { ru, en } = LANG_VALUES;
-  // const { light, dark } = THEME_VALUES;
-  // const lang = isRu ? ru : en;
-  // const theme = isLight ? light : dark;
-  const isRu = lang === 'ru';
+export default function LangSwitch({ dispatch, lang }: SettingsProps) {
+  const isRu = lang === LANG_VALUES.ru;
 
   return (
-    <div className={`${style.langSwitch} ${theme}`} onClick={() => dispatch(changeLanguage())}>
+    <div className={`${style.LangSwitch}`} onClick={() => dispatch(changeLanguage())}>
       <div className={`${style['select-lang-en']} ${!isRu ? style.active : ''}`}>
         <RiTranslate2 className={`${style.icon}`} />
         {`${langsData[lang].menuSettings.enBtn}`}
