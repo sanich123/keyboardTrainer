ddf51addecd6a1ac0ca5adde3da2741cb9979a62
Maxim Predybailo <predmaxim@ya.ru>
2023-02-07 12:25:19 +0500

    refactor: clean code for create-game page

--- src/pages/create-game/create-game.tsx
+++ src/pages/create-game/create-game.tsx
@@ -1,13 +1,8 @@
 import Settings from '../../components';
-import { THEME_VALUES } from '../../utils/const';
-import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 
 export default function CreateGamePage() {
-  const { isLight } = useThemeLang();
-  const { light, dark } = THEME_VALUES;
-  const theme = isLight ? light : dark;
   return (
-    <div className={theme}>
+    <div id="CreateGamePage">
       <Settings />
     </div>
   );
