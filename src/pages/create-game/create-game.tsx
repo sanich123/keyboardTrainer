d968422cb03a764b07115994d8b4c13c0affebcb
Maxim Predybailo <predmaxim@ya.ru>
2023-02-07 19:37:47 +0500

    feat: add TrafficLight on create-game page

--- src/pages/create-game/create-game.tsx
+++ src/pages/create-game/create-game.tsx
@@ -1,9 +1,37 @@
-import Settings from '../../components';
+import Settings, { TrafficLight } from '../../components';
+import { TrafficLightProps } from '../../components/TrafficLight';
+import styles from './create-game.module.scss';
 
 export default function CreateGamePage() {
+  const redProps: TrafficLightProps = {
+    color: 'red',
+    textInfo: '01:22',
+    textDesc: 'Elapsed time',
+    unit: 'min',
+  };
+
+  const greenProps: TrafficLightProps = {
+    color: 'green',
+    textInfo: '120',
+    textDesc: 'Speed',
+    unit: 'chars/min',
+  };
+
+  const yellowProps: TrafficLightProps = {
+    color: 'yellow',
+    textInfo: '100%',
+    textDesc: 'Accuracy',
+    unit: '%',
+  };
+
   return (
     <div id="CreateGamePage">
       <Settings />
+      <div className={`trafficLight ${styles.trafficLight}`}>
+        <TrafficLight {...redProps} />
+        <TrafficLight {...greenProps} />
+        <TrafficLight {...yellowProps} />
+      </div>
     </div>
   );
 }
