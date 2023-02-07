8016fe48ae2c6c84feb1324b201d54d34a6b5fef
Maxim Predybailo <predmaxim@ya.ru>
2023-02-07 21:31:00 +0500

    refactor: TrafficLight
    Change unit position and color

--- src/components/TrafficLight/TrafficLight.tsx
+++ src/components/TrafficLight/TrafficLight.tsx
@@ -2,27 +2,26 @@ import React from 'react';
 import styles from './TrafficLight.module.scss';
 
 export interface TrafficLightProps {
+  id: number,
   color: 'red' | 'green' | 'yellow',
   textInfo: string,
   textDesc: string,
   unit: string,
 }
 
-export function TrafficLight({ color, textInfo, textDesc, unit }: TrafficLightProps) {
+export function TrafficLight({ id, color, textInfo, textDesc, unit }: TrafficLightProps) {
   return (
     <div className={`${styles.TrafficLight} TrafficLight-${color}`}>
       <div className={styles[`${color}Light`]}>
-        <div className={styles.unitWrapper}>
-          <p className={styles[`${color}TextDesc`]}>
-            {textDesc}
-          </p>
-          <p className={styles.unit}>
-            {unit}
-          </p>
-        </div>
+        <p className={styles[`${color}TextDesc`]}>
+          {textDesc}
+        </p>
         <div className={styles[color]}>
           <p className={styles[`${color}TextInfo`]}>
             {textInfo}
+          </p>
+          <p className={styles.unit}>
+            {unit}
           </p>
         </div>
       </div>
