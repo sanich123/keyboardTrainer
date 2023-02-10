8b0330833ceca4712734517757af501e100f9844
Maxim Predybailo <predmaxim@ya.ru>
2023-02-11 00:03:19 +0500

    feat: add TextWindow to game page

--- src/pages/create-game/create-game.tsx
+++ src/pages/create-game/create-game.tsx
@@ -1,11 +1,14 @@
-import Settings, { TrafficLight } from '../../components';
+import Settings, { TextWindow, TrafficLight } from '../../components';
 import { langsData } from '../../components/Settings';
+import { TextWindowProps } from '../../components/TextWindow';
 import { TrafficLightProps } from '../../components/TrafficLight';
 import { LANG_VALUES } from '../../utils/const';
 import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 import styles from './create-game.module.scss';
 
 export default function CreateGamePage() {
+  // document.body.;
+
   const { isRu } = useThemeLang();
   const lang = isRu ? LANG_VALUES.ru : LANG_VALUES.en;
 
@@ -34,14 +37,20 @@ export default function CreateGamePage() {
   };
 
   const trafficLightProps: TrafficLightProps[] = [elapsedTime, speed, accuracy];
-
+  const textWindowProps: TextWindowProps = {
+    lessonText: 'Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor! Lorem ipsum dolor!\n',
+  };
 
   return (
-    <div id="CreateGamePage">
+    <div id="CreateGamePage" className={`${styles.CreateGamePage}`}>
+
       <Settings />
+
       <div className={`trafficLight ${styles.trafficLight}`}>
         {trafficLightProps.map((prop: TrafficLightProps) => <TrafficLight key={`${prop.id}`} {...prop} />)}
       </div>
+
+      <TextWindow {...textWindowProps} />
     </div>
   );
 }
