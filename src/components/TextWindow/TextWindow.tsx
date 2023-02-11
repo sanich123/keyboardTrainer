b5855127c6794dccec94bf4229ec70c129c325a1
Maxim Predybailo <predmaxim@ya.ru>
2023-02-11 18:26:31 +0500

    feat: add some features
    - new props
    - scrol to view
    - fix bugs with the end of the game
    - refactor some funcs

--- src/components/TextWindow/TextWindow.tsx
+++ src/components/TextWindow/TextWindow.tsx
@@ -1,5 +1,8 @@
 import { useRef, useState } from 'react';
+import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 import { getRandomNum } from '../../utils/getRandomNum';
+import { LANG_VALUES } from '../../utils/const';
+import { langsData } from '../Settings';
 import { keys } from './specialKeys';
 import styles from './TextWindow.module.scss';
 
@@ -11,12 +14,16 @@ export interface TextWindowProps {
 export function TextWindow({ lessonText }: TextWindowProps) {
   const randomIterableKey = getRandomNum(0, 10000000000000);
 
-  const setCharToJSX = (char: string, classNames: string): JSX.Element =>
-    <span key={randomIterableKey()} className={classNames}>{char}</span>;
+  const setCharToJSX = (char: string, classNames: string, ref?: React.RefObject<HTMLSpanElement>): JSX.Element =>
+    <span key={randomIterableKey()} className={classNames} ref={ref}>{char}</span>;
 
   const TextWindowReference = useRef<HTMLDivElement>(null);
   const outFocusReference = useRef<HTMLDivElement>(null);
   const textAreaReference = useRef<HTMLDivElement>(null);
+
+  const { isRu } = useThemeLang();
+  const lang = isRu ? LANG_VALUES.ru : LANG_VALUES.en;
+  const { gameOverMessage, startMessage, continueMessage } = (langsData[lang].pageGame.textWindow as { [key: string]: string });
 
   const textJsxArr: JSX.Element[] = lessonText.split('').map((char: string, i): JSX.Element =>
     i === 0 ? setCharToJSX(char, `${styles.text} ${styles.blink}`) : setCharToJSX(char, styles.text));
@@ -25,18 +32,16 @@ export function TextWindow({ lessonText }: TextWindowProps) {
   const [idx, setIdx] = useState(0);
   const [isStoped, setIsStoped] = useState(false);
   const [focused, setFocused] = useState(false);
-  // const [time, setTime] = useState(0);
+  const blinkReference = useRef<HTMLSpanElement>(null);
 
 
-  // const scroll = (target: Ref<T>) =>
-  //   (target as T).scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-
-  // const setBlinkCarret = (char: string, status: string) => {
-  //   char
-  // };
+  const scroll = (): void => {
+    if (blinkReference.current !== null) {
+      blinkReference.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    }
+  };
 
   const checkKey = (e: React.KeyboardEvent<HTMLDivElement>): boolean | undefined | string => {
-    if (text.length === idx) { return 'endGame'; }
     if (e.code === 'Backspace') { return 'Backspace'; }
     if (keys.some((specialKey: string) => specialKey === e.key)) { return 'isSpecialKey'; }
     return e.key === (text[idx].props.children as string);
@@ -46,26 +51,35 @@ export function TextWindow({ lessonText }: TextWindowProps) {
     // eslint-disable-next-line no-nested-ternary
     const i = isBack ? (idx > 0 ? idx - 1 : 0) : idx;
     const before = text.slice(0, i).map((el: JSX.Element) => setCharToJSX(el.props.children, styles.right));
-    const current = setCharToJSX(text[i].props.children, isBack ? `${className} ${styles.blink}` : className);
-    const next = setCharToJSX(text[i + 1].props.children, isBack ? styles.text : `${styles.text} ${styles.blink}`);
-    const after = text.slice(i + 2).map((jsxChar: JSX.Element) => setCharToJSX(jsxChar.props.children, styles.text));
-    const newText = [...before, current, next, ...after];
-    return newText;
+
+    if (text[i + 2] && text.slice(i + 3)) {
+      const current = setCharToJSX(text[i].props.children, isBack ? `${className} ${styles.blink}` : className);
+      const next = setCharToJSX(text[i + 1].props.children, isBack ? styles.text : `${styles.text} ${styles.blink}`);
+      const toScroll = setCharToJSX(text[i + 2].props.children, isBack ? styles.text : styles.text, blinkReference);
+      const after = text.slice(i + 3).map((jsxChar: JSX.Element) => setCharToJSX(jsxChar.props.children, styles.text));
+      const newText = [...before, current, next, toScroll, ...after];
+      return newText;
+    } if (text[i + 1]) {
+      const current = setCharToJSX(text[i].props.children, isBack ? `${className} ${styles.blink}` : className);
+      const next = setCharToJSX(text[i + 1].props.children, isBack ? styles.text : `${styles.text} ${styles.blink}`);
+      const newText = [...before, current, next];
+      return newText;
+    } else {
+      const current = setCharToJSX(text[i].props.children, styles.right);
+      const newText = [...before, current];
+      setGameOver();
+      return newText;
+    }
   };
 
   const onKeyUpHandle = (e: React.KeyboardEvent<HTMLDivElement>): void => {
-    if (checkKey(e) === 'endGame') {
-      gameEnd();
-      return;
-    }
-
     if (checkKey(e) === 'isSpecialKey') { return; }
 
     if (isStoped === false && checkKey(e) === true) {
       setIdx(idx + 1);
       const newText = setNewText(styles.right);
       setText(newText);
-      // scroll();
+      scroll();
       return;
     }
 
@@ -75,6 +89,7 @@ export function TextWindow({ lessonText }: TextWindowProps) {
       const newText = setNewText(styles.text, true);
       setText(newText);
       setIsStoped(false);
+      scroll();
       return;
     }
 
@@ -85,12 +100,11 @@ export function TextWindow({ lessonText }: TextWindowProps) {
     setErrorCount(errorCount + 1);
     setIdx(idx + 1);
     setIsStoped(true);
-    // scroll();
   };
 
-  const gameEnd = (): void => {
+  const setGameOver = (): void => {
+    if (outFocusReference.current !== null) { outFocusReference.current.textContent = gameOverMessage; }
     setIsStoped(true);
-    (outFocusReference.current as HTMLDivElement).textContent = 'Game Over';
     setFocused(false);
   };
 
@@ -100,33 +114,11 @@ export function TextWindow({ lessonText }: TextWindowProps) {
         textAreaReference.current.focus();
         setFocused(true);
       } else {
-        outFocusReference.current.textContent = 'Click me to continue';
+        outFocusReference.current.textContent = continueMessage;
         setFocused(false);
       }
     }
   };
-
-  // const setGameTimer = (status: 'start' | 'stop' | 'pause' | 'unpause'): void => {
-  //   if (status === 'start') {
-  //     setTime(0);
-  //   }
-  //   if (status === 'stop') {
-  //     setTime(0);
-  //   }
-  //   if (status === 'pause') {
-  //     setTime(time);
-  //   }
-  //   if (status === 'unpause') {
-  //     setTime(0);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const area = textAreaReference.current;
-  //   if (area !== null) {
-  //     area.scrollTop = area.scrollHeight;
-  //   }
-  // });
 
   return (
     <div
@@ -146,7 +138,7 @@ export function TextWindow({ lessonText }: TextWindowProps) {
         className={focused ? styles.outFocus : styles.outFocus__active}
         onClick={setFocusToTextWindow}
       >
-        Click me to start
+        {startMessage}
       </div>
     </div>
   );
