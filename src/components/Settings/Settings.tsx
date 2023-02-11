bbe0454917723df846da4d7206d666c10158ee49
Maxim Predybailo <predmaxim@ya.ru>
2023-02-11 18:20:37 +0500

    refactor: Settings component. change props

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import { Dispatch, useState } from 'react';
 import LangSwitch, { langsData } from './LangSwitch';
 import ThemeSwitch from './ThemeSwitch';
 import { LANG_VALUES, THEME_VALUES } from '../../utils/const';
 import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 import { RiSettings2Fill } from 'react-icons/ri';
 import styles from './Settings.module.scss';
+import { AnyAction } from '@reduxjs/toolkit';
+
+export interface SettingsProps {
+  dispatch: Dispatch<AnyAction>,
+  lang: string,
+  theme: string,
+}
 
 export default function Settings() {
   const [show, setShow] = useState(false);
   const { dispatch, isRu, isLight } = useThemeLang();
-  const { ru, en } = LANG_VALUES;
-  const { light, dark } = THEME_VALUES;
-  const lang = isRu ? ru : en;
-  const theme = isLight ? light : dark;
+  const lang = isRu ? LANG_VALUES.ru : LANG_VALUES.en;
+  const theme = isLight ? THEME_VALUES.light : THEME_VALUES.dark;
   const active = show ? styles.active : '';
 
   return (
