842166a9560e79043763731e082be3bfa3aec2bd
Alexander Voronin <aovoronin.piano@gmail.com>
2023-02-13 23:10:34 +0300

    feat: write a bunch of tests and solve peer dependencies problems

---
+++ src/components/best-results/best-results-components.test.tsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../tests/test-utils';
+import { BestIndex } from './best-results-components';
+
+describe('Best-result-components', () => {
+  it('Should correctly render', () => {
+    renderWithProviders(
+      <BestIndex
+        name="Какое-то имя"
+        span={'№'}
+        color={'#FF9C9C'}
+        result={585}
+      />,
+    );
+    expect(screen.getByText(/какое-то имя/i)).toBeInTheDocument();
+    expect(screen.getByText(/585/i)).toBeInTheDocument();
+    expect(screen.getByText(/№/i)).toBeInTheDocument();
+  });
+});
--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -7,18 +7,7 @@ import Header from './header';
 jest.mock('@auth0/auth0-react');
 
 describe('Header', () => {
-  it('Header should render correct elements, when user-info isLoading', () => {
-    (useAuth0 as jest.Mock).mockReturnValue(isLoading);
-    renderWithProviders(<Header/>);
-    expect(screen.getByAltText(/settings/i)).toBeInTheDocument();
-    expect(screen.getAllByRole('link')).toHaveLength(5);
-    expect(screen.getByRole('button')).toBeInTheDocument();
-    expect(screen.getAllByRole('img')).toHaveLength(2);
-    expect(screen.getByRole('navigation')).toBeInTheDocument();
-    expect(screen.getAllByRole('listitem')).toHaveLength(3);
-    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
-  });
-  it('Header should render correct elements, when user-info isAuthenticated', () => {
+  it('Header should render correct elements, when authorization is loading', () => {
     (useAuth0 as jest.Mock).mockReturnValue(isLoading);
     renderWithProviders(<Header />);
     expect(screen.getByAltText(/settings/i)).toBeInTheDocument();
@@ -28,5 +17,27 @@ describe('Header', () => {
     expect(screen.getByRole('navigation')).toBeInTheDocument();
     expect(screen.getAllByRole('listitem')).toHaveLength(3);
     expect(screen.getByText(/loading.../i)).toBeInTheDocument();
+  });
+  it('Header should render correct elements, when user is authorizated', () => {
+    (useAuth0 as jest.Mock).mockReturnValue(authorized);
+    renderWithProviders(<Header />);
+    expect(screen.getByAltText(/settings/i)).toBeInTheDocument();
+    expect(screen.getAllByRole('link')).toHaveLength(5);
+    expect(screen.getAllByRole('button')).toHaveLength(2);
+    expect(screen.getAllByRole('img')).toHaveLength(2);
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(3);
+    expect(screen.getByText(/johndoe@me.com/i)).toBeInTheDocument();
+  });
+  it('Header should render correct elements, when user is not authorizated', () => {
+    (useAuth0 as jest.Mock).mockReturnValue(nonAuthorized);
+    renderWithProviders(<Header />);
+    expect(screen.getByAltText(/settings/i)).toBeInTheDocument();
+    expect(screen.getAllByRole('link')).toHaveLength(5);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getAllByRole('img')).toHaveLength(2);
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(3);
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
   });
 });
