2a7ecdc650b13e4caca55bf17bd2d7617760bdcc
Maxim Predybailo <predmaxim@ya.ru>
2023-02-16 01:40:07 +0500

    feat: add some features
    - add timer function
    - add some props to trafficLightProps, keyboardProps and textWindowProps
    - fix some minor bugs

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -10,8 +10,8 @@ import styles from './Game.module.scss';
 
 export function Game() {
   const text = {
-    en: 'Lorem, .!?-:"`',
-    ru: 'Пре !,.-:"',
+    en: 'Lorem',
+    ru: 'Пре',
   };
   const { isRu } = useThemeLang();
   const lang = isRu ? LANG_VALUES.ru : LANG_VALUES.en;
@@ -21,60 +21,23 @@ export function Game() {
   const [isRightKey, setIsRightKey] = useState(false);
   const [idx, setIdx] = useState(-1);
   const [keyLang, setKeyLang] = useState<'ru' | 'en'>('en');
-  // const charToCode = (char) => {
-  //   const code = char
-  //   return charKey;
-  // }
-  const trafficLightProps: TrafficLightProps[] = [
-    {
-      id: 1,
-      color: 'red',
-      textInfo: '01:22',
-      textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).elapsedTime}`,
-      unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).elapsedTimeUnit}`,
-    },
-    {
-      id: 2,
-      color: 'yellow',
-      textInfo: '100',
-      textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).speed}`,
-      unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).speedUnit}`,
-    },
-    {
-      id: 3,
-      color: 'green',
-      textInfo: '120',
-      textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).accuracy}`,
-      unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).accuracyUnit}`,
-    },
-  ];
+  const [time, setTime] = useState(0);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [speed, setSpeed] = useState(0);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [accuracy, setAccuracy] = useState(0);
 
-  const keyboardProps: KeyboardProps = {
-    lang: lang,
-    char: text[keyLang][idx + 1],
-    isRightKey: isRightKey,
-    idx: idx,
-    setIdx: setIdx,
-    keyLang: keyLang,
-    setKeyLang: setKeyLang,
-  };
-
-  const textWindowProps: TextWindowProps = {
-    lessonText: text[keyLang],
-    lang: lang,
-    idx: idx,
-    isRightKey: isRightKey,
-    isGame: isGame,
-    setIsGame: setIsGame,
-  };
-
-  const checkKey = (e: React.KeyboardEvent<HTMLDivElement>): boolean | string => {
+  const checkKey = (e: React.KeyboardEvent<HTMLDivElement>)
+    : boolean | string => {
     if (e.code === 'Backspace') { return 'Backspace'; }
-    if (keys.some((specialKey: string) => specialKey === e.key)) { return 'isSpecialKey'; }
+    if (keys.some((specialKey: string) => specialKey === e.key)) {
+      return 'isSpecialKey';
+    }
     return e.key === text[keyLang][idx + 1];
   };
 
-  const onKeyUpHandle = (e: React.KeyboardEvent<HTMLDivElement>): void | undefined => {
+  const onKeyUpHandle = (e: React.KeyboardEvent<HTMLDivElement>)
+    : void | undefined => {
     if (isGame === false) {
       return;
     }
@@ -108,7 +71,82 @@ export function Game() {
 
     if (checkKey(e) && idx >= text[keyLang].length - 2) {
       setIsGame(false);
+      // setTime(0);
     }
+  };
+
+  const setTimeToString = (
+    min = 0,
+    minPref = 0,
+    sec = 0,
+    secPref = 0,
+    minRes = '',
+    secRes = '',
+  ) => {
+    sec = time;
+    min = Math.floor(time / 60);
+    sec = sec >= 60 ? time - (min * 60) : time;
+    minRes = min < 10 ? `${minPref}${min}` : `${min}`;
+    secRes = sec < 10 ? `${secPref}${sec}` : `${sec}`;
+    console.log(`${minRes}:${secRes}`, time);
+    return `${minRes}:${secRes}`;
+  };
+
+  const trafficLightProps: TrafficLightProps[] = [
+    {
+      id: 'timer',
+      color: 'red',
+      textInfo: setTimeToString(),
+      textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).elapsedTime}`,
+      unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).elapsedTimeUnit}`,
+    },
+    {
+      id: 'speed',
+      color: 'yellow',
+      textInfo: speed,
+      textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).speed}`,
+      unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).speedUnit}`,
+    },
+    {
+      id: 'accuracy',
+      color: 'green',
+      textInfo: accuracy,
+      textDesc: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).accuracy}`,
+      unit: `${(langsData[lang].pageGame.trafficLight as { [key: string]: string }).accuracyUnit}`,
+    },
+  ];
+
+  const keyboardProps: KeyboardProps = {
+    char: text[keyLang][idx + 1],
+    lang,
+    isRightKey,
+    idx,
+    setIdx,
+    keyLang,
+    setKeyLang,
+    setTime,
+  };
+
+  const textWindowProps: TextWindowProps = {
+    lessonText: text[keyLang],
+    lang,
+    idx,
+    isRightKey,
+    isGame,
+    setIsGame,
+    setTime,
+    setAccuracy,
+    setSpeed,
+    errorCount,
+    time,
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const userData = {
+    date: new Date().toLocaleTimeString(),
+    time,
+    speed,
+    accuracy,
   };
 
   return (
