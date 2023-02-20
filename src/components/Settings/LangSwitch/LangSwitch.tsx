ef2148f7aab6277b0e51349964a8347f878bfa03
Maxim Predybailo <predmaxim@ya.ru>
2023-02-20 12:57:54 +0500

    refactor: change styles names

--- src/components/Settings/LangSwitch/LangSwitch.tsx
+++ src/components/Settings/LangSwitch/LangSwitch.tsx
@@ -1,24 +1,24 @@
 import { changeLanguage } from '../../../redux/global-state/global-state';
 import { langsData } from './langsData';
 import { RiTranslate2 } from 'react-icons/ri';
-import style from './LangSwitch.module.scss';
 import { LANG_VALUES } from '../../../utils/const';
 import { SettingsProps } from '../Settings';
+import style from './LangSwitch.module.scss';
 
 export default function LangSwitch({ dispatch, lang }: SettingsProps) {
   const isRu = lang === LANG_VALUES.ru;
   const { enBtn, ruBtn } = langsData[lang].menuSettings;
   return (
     <div className={`${style.LangSwitch}`} onClick={() => dispatch(changeLanguage())}>
-      <div className={`${style['select-lang-en']} ${!isRu ? style.active : ''}`}>
+      <div className={`${style.selectLangEn} ${!isRu ? style.active : ''}`}>
         <RiTranslate2 className={`${style.icon}`} />
         {`${enBtn}`}
       </div>
-      <div className={`${style['select-lang-en']} ${isRu ? style.active : ''}`}>
-        <RiTranslate2 className={`${style.icon}`} />
+      <div className={`${style.selectLangEn} ${isRu ? style.active : ''}`}>
+        < RiTranslate2 className={`${style.icon}`} />
         {`${ruBtn}`}
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
