5f369d03046e89e62934f7cb1ce10b76ceba424c
Maxim Predybailo <predmaxim@ya.ru>
2023-02-22 17:59:54 +0500

    fix: space key scroll

--- src/pages/create-game/create-game.tsx
+++ src/pages/create-game/create-game.tsx
@@ -2,6 +2,13 @@ import { Game, Header, Footer } from '../../components';
 import styles from './create-game.module.scss';
 
 export default function CreateGamePage() {
+
+  window.addEventListener('keydown', (e: KeyboardEvent) => {
+    if (e.code === 'Space' && e.target === document.body) {
+      e.preventDefault();
+    }
+  });
+
   return (
     <>
       <Header />
