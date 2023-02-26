93bdc9292ad686741cf75568fc6d7233cd0ca197
Alexander Voronin <aovoronin.piano@gmail.com>
2023-02-26 23:15:31 +0300

    feat: finally solved modal window problem

--- src/components/modal-rules/modal.tsx
+++ src/components/modal-rules/modal.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from 'react';
 import { LANG_VALUES } from '../../utils/const';
-import { addEscListener } from '../../utils/event-listeners';
 import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 import { langsData } from '../Settings';
 import './modal.scss';
+import { FocusOn } from 'react-focus-on';
 
 type Modal = {
   setModal: (arg: boolean) => void
@@ -13,34 +12,26 @@ export default function ModalRules({ setModal }: Modal) {
   const { isRu, isLight } = useThemeLang();
   const { ru, en } = LANG_VALUES;
   const lang = isRu ? ru : en;
-  useEffect(() => {
-    addEscListener(setModal);
-    const body = document.querySelector('body');
-    if (body) {
-      body.style.overflow = 'hidden';
-      body.inert = true;
-    }
-    const modal = document.querySelector('.modal-all') as HTMLDivElement;
-    if (modal) {
-      modal.setAttribute('tabindex', '0');
-      modal.focus();
-    }
-  });
 
   return (
-    <div className="modal-all" onClick={() => setModal(false)}>
-      <div
-        className={`modal-container ${isLight ? '' : 'darck-modal'}`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="modal-header">
-          <h1 className="h1-modal">{`${langsData[lang].modal.modalName}`}</h1>
-          <button className="span-close" onClick={() => setModal(false)} onFocus={() => console.log('Я в фокусе')}>
-              &#x2718;
-          </button>
+    <FocusOn onEscapeKey={() => setModal(false)}>
+      <div className="modal-all" onClick={() => setModal(false)}>
+        <div
+          className={`modal-container ${isLight ? '' : 'darck-modal'}`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="modal-header">
+            <h1 className="h1-modal">{`${langsData[lang].modal.modalName}`}</h1>
+            <button
+              className="span-close"
+              onClick={() => setModal(false)}
+            >
+                &#x2718;
+            </button>
+          </div>
+          <p className="p-modal-text">{`${langsData[lang].modal.modalText}`}</p>
         </div>
-        <p className="p-modal-text">{`${langsData[lang].modal.modalText}`}</p>
       </div>
-    </div>
+    </FocusOn>
   );
 }
