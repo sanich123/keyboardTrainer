7156cc5096e3d2f8c9747ca40fea41c0b3f44b1b
sanich123 <aovoronin.piano@gmail.com>
2023-02-26 18:39:06 +0300

    feat: work on accessibility

--- src/pages/create-main/create-main.tsx
+++ src/pages/create-main/create-main.tsx
@@ -4,20 +4,13 @@ import CtaElement from '../../components/cta-element/cta-element';
 import styles from './create-main.module.scss';
 import { useState } from 'react';
 import ModalRules from '../../components/modal-rules/modal';
-import { FocusOn } from 'react-focus-on';
-
 
 export default function CreateMainPage() {
-
   const [ modal, setModal ] = useState(false);
 
   return (
     <>
-      {modal && (
-        <FocusOn onEscapeKey={() => setModal(false)}>
-          <ModalRules setModal={setModal} />
-        </FocusOn>
-      )}
+      {modal && <ModalRules setModal={setModal} />}
       <main className={`base ${styles.mainpageDiv}`}>
         <Header />
         <CtaElement setModal={setModal} />
