db0415bc9ae989d4257d0d012ddac3b46039a558
sanich123 <aovoronin.piano@gmail.com>
2023-02-27 16:35:59 +0300

    feat: finally done with modal

--- src/components/modal-rules/modal.tsx
+++ src/components/modal-rules/modal.tsx
@@ -2,7 +2,10 @@ import { LANG_VALUES } from '../../utils/const';
 import { useThemeLang } from '../../utils/hooks/use-theme-lang/use-theme-lang';
 import { langsData } from '../Settings';
 import './modal.scss';
-import { FocusOn } from 'react-focus-on';
+import { useEffect } from 'react';
+import { RemoveScroll } from 'react-remove-scroll';
+import FocusLock from 'react-focus-lock';
+import { addEscListener } from '../../utils/event-listeners';
 
 type Modal = {
   setModal: (arg: boolean) => void
@@ -12,26 +15,28 @@ export default function ModalRules({ setModal }: Modal) {
   const { isRu, isLight } = useThemeLang();
   const { ru, en } = LANG_VALUES;
   const lang = isRu ? ru : en;
+  useEffect(() => {
+    addEscListener(setModal);
+  });
 
   return (
-    <FocusOn onEscapeKey={() => setModal(false)}>
-      <div className="modal-all" onClick={() => setModal(false)}>
-        <div
-          className={`modal-container ${isLight ? '' : 'darck-modal'}`}
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className="modal-header">
-            <h1 className="h1-modal">{`${langsData[lang].modal.modalName}`}</h1>
-            <button
-              className="span-close"
-              onClick={() => setModal(false)}
-            >
+    <RemoveScroll removeScrollBar={false}>
+      <FocusLock>
+        <div className="modal-all" onClick={() => setModal(false)}>
+          <div
+            className={`modal-container ${isLight ? '' : 'darck-modal'}`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="modal-header">
+              <h1 className="h1-modal">{`${langsData[lang].modal.modalName}`}</h1>
+              <button className="span-close" onClick={() => setModal(false)}>
                 &#x2718;
-            </button>
+              </button>
+            </div>
+            <p className="p-modal-text">{`${langsData[lang].modal.modalText}`}</p>
           </div>
-          <p className="p-modal-text">{`${langsData[lang].modal.modalText}`}</p>
         </div>
-      </div>
-    </FocusOn>
+      </FocusLock>
+    </RemoveScroll>
   );
 }
